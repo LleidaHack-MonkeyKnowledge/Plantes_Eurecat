8dd17161dc1507f695c83a420f5d61b428b496b2
MaralGS <algarselma@gmail.com>
2023-11-25 13:18:15 +0100

    Connection Done

--- Scripts/connection.py
+++ Scripts/connection.py
@@ -2,12 +2,12 @@ from paho.mqtt import client as mqtt
 
 broker = '84.88.76.18'
 port = 1883
-topic = "hackeps/RA" #"hackeps/eurecat"
+topic = "hackeps/eurecat" #"hackeps/eurecat"
 user = 'regimiento_autoguardia'
 password = 'R3g1m3nt@gr0gu4rd14'
 client_id = 'MonkeyKnowledge'
 
-def connect_mqtt():
+def connect_mqtt() -> mqtt:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
@@ -18,4 +18,27 @@ def connect_mqtt():
     # client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
+    #if client.is_connected:
+    #    print("CONNEECCtED!!")
+    #else:
+    #    print("LLORASION")
     return client
+
+def subscribe(client: mqtt):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
+#______________
+#   MAIN
+#______________
+
+run()
