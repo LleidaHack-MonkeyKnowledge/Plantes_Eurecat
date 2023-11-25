b70fd21ed4fa689b786b1a5aeb1a075152bd5e96
David Benages <79161120+Divangus@users.noreply.github.com>
2023-11-25 15:10:15 +0100

    Update saveToCsv.py

--- Scripts/saveToCsv.py
+++ Scripts/saveToCsv.py
@@ -1,8 +1,20 @@
 import csv
 import numpy as np
+import pandas as pd
+import json
 
-tabla = np.random.random((3000,14))
+#tabla = np.random.random((3000,14))
 
-with open('data/data.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows(tabla)
+# Reading JSON data from a file
+with open("data/data.json") as f:
+    json_data = json.load(f)
+
+# Converting JSON data to a pandas DataFrame
+df = pd.read_json(json_data)
+
+# Writing DataFrame to a CSV file
+df.to_csv("data/data.csv", index=False)
+
+#with open('data/data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+ #   writer = csv.writer(csvfile)
+  #  writer.writerows(tabla)
